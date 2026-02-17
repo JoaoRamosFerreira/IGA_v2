ae64a2b30026524884c5a4f3eb907d3570f2c04a
JoaoRamosFerreira <131286321+JoaoRamosFerreira@users.noreply.github.com>
2026-02-17 16:17:10 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/IGA_v2/', // Ensure 'IGA' is uppercase if your repo is 'IGA_v2'
   plugins: [react()],
-  base: '/IGA_v2/', // must match repo name exactly
-})
+});
